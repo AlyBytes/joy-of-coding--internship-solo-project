8a31d5fd8a7ae38f22057b2995b8fe2968743bc6
AlyCoding <aly.khamidullina@gmail.com>
2025-04-22 07:11:52 -0700

    new components folder to extract error msg

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,18 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { describe } from "node:test";
-import {z} from 'zod'
 import {prisma}from "@/prisma/client"
-
-const createTaskSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1).max(255)
-})
+import { createTaskSchema } from "../../validationSchemas";
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
     const validation = createTaskSchema.safeParse(body)
     if (!validation.success)
-        return NextResponse.json(validation.error.errors, {status: 400})
+        return NextResponse.json(validation.error.format(), {status: 400})
 
     const newTask = await prisma.task.create({
         data:{title:body.title, description:body.description}
--- app/tasks/new/page.tsx
+++ app/tasks/new/page.tsx
@@ -1,40 +1,76 @@
-'use client'
+"use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextArea, TextField } from "@radix-ui/themes";
 // import React from "react";
 import SimpleMDE from "react-simplemde-editor";
-import { useForm, Controller } from "react-hook-form"
-import axios from 'axios'
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createTaskSchema } from "@/app/validationSchemas";
+import { z } from "zod";
+import { ErrorMessage } from "@/app/components/ErrorMessage";
 
-interface TaskForm{
-    title: string,
-    description: string
-}
+type TaskForm = z.infer<typeof createTaskSchema>;
+//we're implementing client side validation with help of zod and axios
+// interface TaskForm {  ---> we are letting Zod infer this type based on this schema
+//   title: string;    ----> z.infer func returns a type, so we store it in type object
+//   description: string;
+// }
 
 const NewTaskPage = () => {
-    const router = useRouter();
-    const{register, control, handleSubmit} = useForm<TaskForm>();
-    // console.log(register('title'))
+  const router = useRouter();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TaskForm>({
+    resolver: zodResolver(createTaskSchema),
+  });
+  // console.log(register('title'))
+  const [error, setError] = useState("");
+
   return (
-    <form 
-        className='max-w-xl space-y-3' 
-        onSubmit={handleSubmit(async (data)=>{await axios.post('/api/tasks', data);
-            router.push('/tasks')
-        })}>
-        <TextField.Root size="1" placeholder="New Task" {...register('title')}> 
-        {/* <TextArea placeholder="New Task" /> */}
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="max-w-xl space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/tasks", data);
+            router.push("/tasks");
+          } catch (error) {
+            // console.log(error)
+            setError("Unexpected Error");
+          }
+        })}
+      >
+        <TextField.Root size="1" placeholder="New Task" {...register("title")}>
+          {/* <TextArea placeholder="New Task" /> */}
         </TextField.Root>
-      {/* <TextArea placeholder="Description" /> */}
+        {/* <TextArea placeholder="Description" /> */}
+
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+
         <Controller
-            name='description'
-            control={control}
-            render ={({field})=><SimpleMDE placeholder="Description" {...field} />}
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
         />
-      
-      <Button> Submit New Task</Button>
-    </form>
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+
+        <Button> Submit New Task</Button>
+      </form>
+    </div>
   );
 };
 
