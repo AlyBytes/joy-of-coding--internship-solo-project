75a3ca33ed0a2e172183edd0c585fe9d129b45ae
AlyCoding <aly.khamidullina@gmail.com>
2025-04-29 11:17:39 -0700

    Pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
   return (
-    <div> Hello World</div>
+    <>
+      <div> Hello World</div>
+      <Pagination itemCount={100} pageSize={10} currentPage={2} />
+    </>
   );
 }
--- app/tasks/list/page.tsx
+++ app/tasks/list/page.tsx
@@ -3,13 +3,49 @@ import { Button, Table } from "@radix-ui/themes";
 // import Link from "next/link";
 // import { Link } from "@radix-ui/themes"; //when using this component instead of Link from next navigation we lose client side rendering and page reloads every time
 import Link from "../../components/Link";
+import NextLink from 'next/link'
 import { prisma } from "@/prisma/client";
 import TaskStatusBadge from "../../components/TaskStatusBadge";
 // import delay from "delay";
 import TasksActionButton from "./TasksActionButton";
+import { Status, Task } from "@prisma/client";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 
-const TasksPage = async () => {
-  const tasks = await prisma.task.findMany();
+interface Props {
+  searchParams: { status: Status, orderBy:keyof Task };
+}
+const TasksPage = async ({ searchParams }: Props) => {
+  const columns: {
+    label: string;
+    value: keyof Task;
+    className?: string;
+  }[] = [
+    { label: "Task", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+  ];
+  const statuses = Object.values(Status);
+  // console.log(statuses)
+  // console.log(searchParams.status)
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+  const where = { status };
+
+  const orderBy = columns.map(column => column.value).includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
+  // // const page = parseInt(searchParams.page) || 1;
+  // const pageSize = 10;
+
+  // const tasks = await prisma.task.findMany({
+  //   where,
+  //   orderBy:
+  //   {[searchParams.orderBy]:'asc'},
+    // skip: (page - 1) * pageSize,
+    // take: pageSize,
+  // });
   // await delay(2000);
 
   return (
@@ -24,13 +60,24 @@ const TasksPage = async () => {
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Task</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
+            {columns.map((column) => (
+              <Table.ColumnHeaderCell key={column.value} className={column.className}>
+                <NextLink  href={{
+                    query: {
+                      ...searchParams,
+                      orderBy: column.value,
+                    },
+                  }}>{column.label}</NextLink>{column.value === searchParams.orderBy && <ArrowUpIcon className="inline"/>}
+                  Task
+              </Table.ColumnHeaderCell>
+            ))}
+
+            {/* <Table.ColumnHeaderCell className="hidden md:table-cell">
               Status
             </Table.ColumnHeaderCell>
             <Table.ColumnHeaderCell className="hidden md:table-cell">
               Created
-            </Table.ColumnHeaderCell>
+            </Table.ColumnHeaderCell> */}
           </Table.Row>
         </Table.Header>
         <Table.Body>
